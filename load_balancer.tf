308ac677b5ac495e5c3336556c3d2743f8a468fc
Abhishek Kumar <abhishekkumar33118@gmail.com>
2024-08-09 17:14:51 +0530

    Application loadbalancer

---
+++ load_balancer.tf
@@ -0,0 +1,43 @@
+resource "aws_lb" "web_lb" {
+  name               = "web-lb"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.alb_sg.id]
+  subnets            = aws_subnet.public_subnet.*.id
+
+  tags = {
+    Name = "web-lb"
+  }
+}
+
+resource "aws_lb_target_group" "web_tg" {
+  name     = "web-tg"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = aws_vpc.my_vpc.id
+
+  health_check {
+    interval            = 30
+    path                = "/"
+  }
+
+  tags = {
+    Name = "web-tg"
+  }
+}
+
+resource "aws_lb_listener" "web_listener" {
+  load_balancer_arn = aws_lb.web_lb.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.web_tg.arn
+  }
+}
+
+resource "aws_autoscaling_attachment" "asg_attachment" {
+  autoscaling_group_name = aws_autoscaling_group.web_asg.name
+  lb_target_group_arn    = aws_lb_target_group.web_tg.arn
+}
